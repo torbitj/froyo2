a336ebf7245da7f5480c73b3958f90efa9cf3ecb
JT <jstorbit@gmail.com>
2025-11-11 17:48:41 -0700

    Add anonymous function to store and count flavors

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const userOrderInput = prompt(`What flavors of froyo would you like?`);
 const flavorsArray = userOrderInput.split(`,`);
 
-const orderSummary = flavorsArray.reduce((), {});
+const orderSummary = flavorsArray.reduce((orderFlavors, flavor) => {
+  const currentFlavor = orderFlavors[flavor];
+  if (currentFlavor === undefined) {
+    currentFlavor = 1;
+  }
+  else {
+    currentFlavor++;
+  }
+}, {});
